110fd7719e9f2a9cd957bf94182992d3288db810
Roman <ramonvicious@gmail.com>
2023-04-04 19:00:57 +0200

    Initial commit

---
+++ app/src/main/java/ua/edu/sumdu/powerreceiver/CustomReceiver.java
@@ -0,0 +1,40 @@
+package ua.edu.sumdu.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+    private static final String ACTION_CUSTOM_BROADCAST = BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    public static final String EXTRA_INT = "EXTRA_INT_NUMBER";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+
+        if (intentAction != null) {
+            String message = context.getString(R.string.unknown_action);
+            switch (intentAction) {
+                case Intent.ACTION_POWER_CONNECTED:
+                    message = context.getString(R.string.power_connected);
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    message = context.getString(R.string.power_disconnected);
+                    break;
+                case ACTION_CUSTOM_BROADCAST:
+                    if (intent.hasExtra(EXTRA_INT)) {
+                        int number = intent.getIntExtra(EXTRA_INT, 0);
+                        message = context.getString(
+                                R.string.custom_broadcast_toast,
+                                (int) Math.pow(number, 2)
+                        );
+                    }
+                    break;
+            }
+
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/ua/edu/sumdu/powerreceiver/MainActivity.java
@@ -0,0 +1,48 @@
+package ua.edu.sumdu.powerreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private final CustomReceiver mReceiver = new CustomReceiver();
+
+    private static final String ACTION_CUSTOM_BROADCAST = BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        this.registerReceiver(mReceiver, filter);
+
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(mReceiver, new IntentFilter(ACTION_CUSTOM_BROADCAST));
+    }
+
+    public void sendCustomBroadcast(View view) {
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+
+        int number = new Random().nextInt(20);
+        customBroadcastIntent.putExtra(CustomReceiver.EXTRA_INT, number);
+
+        LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        this.unregisterReceiver(mReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+}
